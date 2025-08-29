302917148f321c35036bea6e6e0fbf57df00af8c
André Franchin <dedefranchin@gmail.com>
2025-08-29 15:55:17 -0300

    ok

---
+++ composeApp/src/commonMain/kotlin/com/example/treine_me/models/apiModels.kt
@@ -0,0 +1,9 @@
+package com.example.treine_me.network.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiError(val message: String, val details: String? = null, val field: String? = null)
+
+@Serializable
+data class ApiResponse<T>(val success: Boolean, val data: T? = null, val error: ApiError? = null)
